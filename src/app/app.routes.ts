96c0c9a492dff3660af80a3cb61036b754ade2d3
Yash <dualcoreitsolution@gmail.com>
2025-08-20 14:24:02 +0530

    upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Service } from './pages/service/service';
+import { Technologies } from './pages/technologies/technologies';
+import { Blog } from './pages/blog/blog';
+import { Contact } from './pages/contact/contact';
+import { Career } from './pages/career/career';
+import { Project } from './pages/project/project';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'service', component: Service },
+  { path: 'technologies', component: Technologies },
+  { path: 'blog', component: Blog },
+  { path: 'contact', component: Contact },
+  { path: 'career', component: Career },
+  { path: 'project', component: Project },
+
+
+
+];
